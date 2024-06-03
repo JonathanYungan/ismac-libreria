61ce3b9a8407fa21c510501296a3a7d794edc618
MATUTINO <MATUTINO@DESKTOP-SJ72KKT>
2024-06-03 12:25:32 -0500

    jyv00007 no c que se hace

---
+++ src/com/distribuida/principal/PrincipalCliente.java
@@ -0,0 +1,32 @@
+package com.distribuida.principal;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.distribuida.dao.ClienteDAO;
+import com.distribuida.entities.Cliente;
+
+
+
+
+
+public class PrincipalCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//PATRON IOC INVERSION DEL CONTROL
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+		
+		ClienteDAO clienteDAO = context.getBean("clienteDAOImpl",ClienteDAO.class);
+		
+		List<Cliente>clientes = clienteDAO.findALL();
+				clientes.forEach(item ->{
+					System.out.print(item.toString());
+				});
+		
+		context.close();
+		}
+
+}
