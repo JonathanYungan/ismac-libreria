f059a95afb9caa30b483ec973393f550b7f0c5b0
MATUTINO <MATUTINO@DESKTOP-SJ72KKT>
2024-06-03 12:05:24 -0500

    jy00005CREACION DE IMPORTS

--- src/com/distribuida/dao/ClienteDAOompl.java
+++ src/com/distribuida/dao/ClienteDAOompl.java
@@ -2,19 +2,32 @@ package com.distribuida.dao;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.distribuida.entities.Cliente;
 @Repository
 
 public class ClienteDAOompl implements ClienteDAO {
+	
+	
+	@Autowired
+	private SessionFactory sessionFactory;	
+	
+	
 
 	@Override
+	@Transactional
 	public List<Cliente> findALL() {
 		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();
+		return session.createQuery("from Clinete", Cliente.class).getResultList();
 	}
-
+ 
 	@Override
 	public Cliente findOne(int id) {
 		// TODO Auto-generated method stub
