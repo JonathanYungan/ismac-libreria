ae9cac3a7381ca47947f5a2f8d24667bb0572315
MATUTINO <MATUTINO@DESKTOP-SJ72KKT>
2024-06-05 10:27:06 -0500

    avance de autor

---
+++ src/com/distribuida/principal/PrincipalAutor.java
@@ -0,0 +1,25 @@
+package com.distribuida.principal;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.distribuida.dao.AutorDAO;
+import com.distribuida.entities.Autor;
+
+public class PrincipalAutor {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//PATRON IOC INVERSION DEL CONTROL
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		
+		AutorDAO autorDAO = context.getBean("autorDAOImpl",AutorDAO.class);
+		
+		List<Autor>autor = autorDAO.findALL();
+				autor.forEach(item ->{
+					System.out.println(item.toString());
+				});
+		
+		context.close();
+		}
+}
