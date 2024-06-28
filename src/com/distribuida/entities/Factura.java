6139110fa7c41f0213ed8d62e12d5a8e6958c414
MATUTINO <MATUTINO@DESKTOP-SJ72KKT>
2024-06-28 12:26:48 -0500

    vfactura

--- src/com/distribuida/entities/Factura.java
+++ src/com/distribuida/entities/Factura.java
@@ -1,19 +1,49 @@
 package com.distribuida.entities;
  
-import java.sql.Date;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 @Component
+@Entity
+@Table(name = "factura")
+
+//@OneToOne
+//@ManyToOne
+//@OneToMany
 public class Factura {
  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_factura")
 	private int idFactura;
+	@Column(name = "num_factura")
 	private String numFactura;
+	@Column(name = "fecha")
 	private Date fecha;
+	@Column(name = "total_neto")
 	private double totalNeto;
+	@Column(name = "iva")
 	private double iva;
+	@Column(name = "total")
 	private double total;
+	
+	
 	//private int idCliente;
+	@JoinColumn(name = "id_cliente")
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
 	private Cliente cliente;
+	
+
 	
 	public Factura() {}
  
@@ -28,6 +58,9 @@ public class Factura {
 		this.total = total;
 		//this.cliente = cliente;
 	}
+	
+	
+	
  
 	public int getIdFactura() {
 		return idFactura;
--- src/com/distribuida/principal/PrincipalCliente.java
+++ src/com/distribuida/principal/PrincipalCliente.java
@@ -1,5 +1,6 @@
 package com.distribuida.principal;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -25,32 +26,23 @@ public class PrincipalCliente {
 		//CRUD : CREATE, READ, UPDATE, DELETE
 		
 		//ADD
-		Cliente cliente = new Cliente(0,"1712164381","Naomi","Yanez","mi casa..","0987654321","naomisa@gmail.com");
-		clienteDAO.add(cliente);
+		Cliente Cliente = new Cliente(0,"1712164381","Naomi","Yanez",new Date(),"mi casa..","0987654321","naomisa@gmail.com");
+		//clienteDAO.add(Cliente);
 		
 		//UP
-		Cliente cliente2 = new Cliente(0,"1712164381","Sofia","Yanez","mi casa.. mas adentro","0987654321","Sofia@gmail.com");
-		clienteDAO.up(cliente);
+		Cliente cliente2 = new Cliente(41,"1712164381","zofia","Yanez",new Date(),"mi casa.. mas adentro","0987654321","Sofia@gmail.com");
+		//clienteDAO.up(cliente2);
 		
 		// del
-		//clienteDAO.del(1);
+		clienteDAO.del(41);
 		//findOne
 		
-		clienteDAO.del(1);
+		
 		System.out.println("************************* DEL *************************"+clienteDAO.findOne(1));
 		//finALL
 		//List<Cliente> clientes = clienteDAO,findALL();
 		clienteDAO.findALL().forEach(item -> {System.out.println(item.toString());});
 		
-		
-		
-		
-		
-		
-		List<Cliente>clientes = clienteDAO.findALL();
-				clientes.forEach(item ->{
-					System.out.println(item.toString());
-				});
 		
 		context.close();
 		}
